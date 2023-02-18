bf7c242c4a6085701bffcf860caa746220e059ef
KhrisTheLearner <khris.learns@gmail.com>
2023-02-17 19:50:57 -0800

    edit 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main -main block
- * Description: prints all single digit number of base 10
- * starting from 0, followed by a new line.
- * Return: always 0 (Success)
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`, and only 3 times.
+ * Return: 0
  */
 int main(void)
 {
-	char ch;
+	char c;
+	int i;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(ch);
+		putchar(i + '0');
 	}
-	for (ch = 'a'; ch <= 'f'; ch++)
+	for (c = 'a'; c < 'g'; c++)
 	{
-		putchar(ch);
+		putchar(c);
 	}
 	putchar('\n');
+
 	return (0);
 }
