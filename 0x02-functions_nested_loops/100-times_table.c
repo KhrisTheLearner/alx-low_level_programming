43cbebd0465e468b773d408ec1acdc7daff1fe68
KhrisTheLearner <khris.learns@gmail.com>
2023-02-21 14:50:23 -0800

    Edit file 100 for the nth times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,56 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_times_table - Print the `n` times table, starting with 0.
  * Description: If `n` is greater than 15 or less than 0, print nothing.
  * @n: int type number
  */
+void print_times_table(int n);
+
+int main(void)
+{
+    print_times_table(13);
+}
+
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+    int row = 0, col, val;
 
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z > 99)
-			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
+    if (n > 15 || n < 0)
+        return;
+    while (row <= n)
+    {
+        for (col = 0; col <= n; col++)
+        {
+            val = row * col;
+            if (val > 99)
+            {
+                putchar(val / 100 + '0');
+                putchar(val / 10 % 10 + '0');
+                putchar(val % 10 + '0');
+            }
+            else if (val > 9)
+            {
+                putchar(' ');
+                putchar(val / 10 + '0');
+                putchar(val % 10 + '0');
+            }
+            else if (col != 0)
+            {
+                putchar(' ');
+                putchar(' ');
+                putchar(val + '0');
+            }
+            else
+                putchar(val + '0');
 
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		x++;
-	}
+            if (col != n)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+        row++;
+    }
 }
