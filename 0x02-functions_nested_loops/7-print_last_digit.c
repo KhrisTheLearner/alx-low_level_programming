d7a0e273c878280ee67e9c5eafcf14a56b9e927c
KhrisTheLearner <khris.learns@gmail.com>
2023-02-21 13:48:56 -0800

    3rd modify fi;e 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * print_last_digit - main fuction
  * Description: my description
  * @n: hello
  * Return: 0
  */
+
 int print_last_digit(int n)
 
 {
