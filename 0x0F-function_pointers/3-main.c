084b86390af2f0964e0c1d21a7867957574f4186
KhrisTheLearner <khris.learns@gmail.com>
2023-03-22 16:00:20 -0700

    perform simple ops (update)

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3-calc.h"
+
 /**
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
@@ -19,20 +19,25 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
