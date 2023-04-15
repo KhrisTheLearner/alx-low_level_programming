80e02f1e20b1b1f4992e0d0c208e424aea9405bb
KhrisTheLearner <khris.learns@gmail.com>
2023-04-14 17:08:29 -0700

    update 2nd file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,33 @@
-
 #include "main.h"
 
 /**
- * create_textfile - Generates a new file and writes content to it.
- * @file_name: A pointer to the name of the file to be created.
- * @content: A pointer to the string to be written to the file.
+ * create_file - Creates a file and writes content to it.
+ * @filename: Pointer to the name of the file to be created.
+ * @text_content: Pointer to the string to be written to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_textfile(const char *file_name, char *content)
+int create_file(const char *filename, char *text_content)
 {
-	int file_desc, write_status, content_len = 0;
+	int file_descriptor, write_result, content_length = 0;
 
-	if (file_name == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (content != NULL)
+	if (text_content != NULL)
 	{
-		for (content_len = 0; content[content_len];)
-			content_len++;
+		for (content_length = 0; text_content[content_length];)
+			content_length++;
 	}
 
-	file_desc = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write_status = write(file_desc, content, content_len);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_result = write(file_descriptor, text_content, content_length);
 
-	if (file_desc == -1 || write_status == -1)
+	if (file_descriptor == -1 || write_result == -1)
 		return (-1);
 
-	close(file_desc);
+	close(file_descriptor);
 
 	return (1);
 }
